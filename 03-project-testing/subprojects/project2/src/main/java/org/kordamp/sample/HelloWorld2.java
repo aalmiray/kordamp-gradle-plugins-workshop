8765f6085912f6f243442c3330946f386c92cd6b
aalmiray <aalmiray@gmail.com>
2019-03-28 19:26:09 +0100

    Fix package names

--- 03-project-testing/subprojects/project2/src/main/java/org/kordamp/sample/HelloWorld2.java
+++ 03-project-testing/subprojects/project2/src/main/java/org/kordamp/sample/HelloWorld2.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kordmap.sample;
+package org.kordamp.sample;
 
 public class HelloWorld2 {
     public String sayHello() {
